658c4038307989908b4c5f30f06a90470b4108fb
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-09-20 22:10:19 -0300

    Classe Contexto Adicionada

---
+++ mvc/BikeSegura/BikeSegura/Models/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BikeSegura.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(nameOrConnectionString: "StringConexao") { }
+
+        public DbSet<Bicicletas> Bicicletas { get; set; }
+        public DbSet<Historico> Historico { get; set; }
+        public DbSet<InformacoesRoubo> InformacoesRoubo { get; set; }
+        public DbSet<Marcas> Marcas { get; set; }
+        public DbSet<NumeroSerie> NumeroSerie { get; set; }
+        public DbSet<Pessoas> Pessoas { get; set; }
+        public DbSet<RelatoRoubo> RelatoRoubo { get; set; }
+    }
+}
