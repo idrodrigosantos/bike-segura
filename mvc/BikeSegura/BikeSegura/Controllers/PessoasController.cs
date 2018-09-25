0c402e8498da443511cd7ede63db249baff686d0
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-09-25 16:41:47 -0300

    Controller e Views Adicionado
    Controller e views da classe Pessoas adicionado

---
+++ mvc/BikeSegura/BikeSegura/Controllers/PessoasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BikeSegura.Models;
+
+namespace BikeSegura.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Pessoas
+        public ActionResult Index()
+        {
+            return View(db.Pessoas.ToList());
+        }
+
+        // GET: Pessoas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoas pessoas = db.Pessoas.Find(id);
+            if (pessoas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoas);
+        }
+
+        // GET: Pessoas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,ConfirmaEmail,Senha,ConfirmaSenha,Endereco,Numero,Complemento,Cep,Bairro,Cidade,Estado,Telefone,Celular,Cpf,DataNascimento,Genero")] Pessoas pessoas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pessoas.Add(pessoas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoas);
+        }
+
+        // GET: Pessoas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoas pessoas = db.Pessoas.Find(id);
+            if (pessoas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoas);
+        }
+
+        // POST: Pessoas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,ConfirmaEmail,Senha,ConfirmaSenha,Endereco,Numero,Complemento,Cep,Bairro,Cidade,Estado,Telefone,Celular,Cpf,DataNascimento,Genero")] Pessoas pessoas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pessoas);
+        }
+
+        // GET: Pessoas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoas pessoas = db.Pessoas.Find(id);
+            if (pessoas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoas);
+        }
+
+        // POST: Pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pessoas pessoas = db.Pessoas.Find(id);
+            db.Pessoas.Remove(pessoas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
