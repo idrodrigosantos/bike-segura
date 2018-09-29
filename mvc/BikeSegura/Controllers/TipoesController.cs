bfbe66aae46fad308a5023a790cfe695a76c2c3b
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-09-29 17:01:39 -0300

    CRUD Tipo Bicicletas Criado

---
+++ mvc/BikeSegura/Controllers/TipoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BikeSegura.Models;
+
+namespace BikeSegura.Controllers
+{
+    public class TipoesController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Tipoes
+        public ActionResult Index()
+        {
+            return View(db.Tipo.ToList());
+        }
+
+        // GET: Tipoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tipoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome")] Tipo tipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(tipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // POST: Tipoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Tipo tipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tipo);
+        }
+
+        // GET: Tipoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tipo tipo = db.Tipo.Find(id);
+            if (tipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipo);
+        }
+
+        // POST: Tipoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tipo tipo = db.Tipo.Find(id);
+            db.Tipo.Remove(tipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
