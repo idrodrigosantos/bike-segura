818c9e8adb6ef9cac4f603576e44867090052575
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-10-15 16:08:37 -0300

    Estilo Alterado

--- mvc/BikeSegura/App_Start/BundleConfig.cs
+++ mvc/BikeSegura/App_Start/BundleConfig.cs
@@ -27,7 +27,7 @@ namespace BikeSegura
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/meu-estilo.css",
+                      "~/Content/Estilo.css",
                       "~/Content/all.css"));
         }
     }
