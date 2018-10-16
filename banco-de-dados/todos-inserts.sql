269126ea54b1dce3ee5b429c216acfb2ce88f001
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-10-15 23:07:13 -0300

    Insert Adicionado

---
+++ banco-de-dados/todos-inserts.sql
@@ -0,0 +1,183 @@
+use bikesegura;
+
+insert into aros (Medida) values 
+('Aro 12'), ('Aro 14'), ('Aro 16'), ('Aro 20'), ('Aro 24'), ('Aro 26'), 
+('Aro 27,5'), ('Aro 28'), ('Aro 29'), ('Aro 700'), ('OUTRO');
+
+insert into cambiosdianteiros (Velocidade) values 
+('1 Velocidade'), ('2 Velocidades'), ('3 Velocidades');
+
+insert into cambiostraseiros (Velocidade) values 
+('1 Velocidade'), ('2 Velocidades'), ('3 Velocidades'), ('4 Velocidades'), ('5 Velocidades'), 
+('6 Velocidades'), ('7 Velocidades'), ('8 Velocidades'), ('9 Velocidades'), ('10 Velocidades'), ('11 Velocidades');
+
+insert into freios (Nome) values 
+('Cantilever'), ('V-Break'), ('Freio à disco mecânico'), ('Freio à disco hidráulico'), 
+('Tipo Ferradura / Caliper'), ('Hidráulico'),  ('OUTRO');
+
+insert into quadros (Material) values 
+('Aço'), ('Alumínio'), ('Fibra de Carbono'), ('OUTRO');
+
+insert into suspensoes (Nome) values 
+('Rígida'), ('Eslastômeros'), ('Molas'), ('Hidráulica (Ar/Óleo)'), ('OUTRO');
+
+insert into tipos (Nome) values 
+('BMX / Cross'), ('Dobrável'), ('Downhill'), ('Estrada / Speed / Road / Corrida'), ('Handbike'), ('Híbrida'), ('Infantil'), 
+('Mountain Bike'), ('Triatlo'), ('Urbana'), ('Infantil'), ('Elétrica'), ('Fixa / Fixed Gear Bike / Bike Fixa'), ('OUTRO');
+
+insert into marcas (Nome) values
+('24SEVEN'), ('4EVER'), ('88'), ('ABICI'), ('ABSOLUTE'), ('ACADEMY'), ('ADVENTURE'), ('AEGIS'), ('AEROTECH'), ('AIRBORNE'), 
+('AIRWALK'), ('AKRON'), ('ALEOCA'), ('ALFAMEQ'), ('ALL-CITY'), ('AMF GLORIA MILANO'), ('APEX'), ('APOLLO'), ('ARAYA'), 
+('ARGON 18'), ('AROPEC'), ('ASPRILLE'), ('ASTRO'), ('ATALA'), ('ATHOR'), ('AUDAX'), ('AUTHOR'), ('AVALON'), ('AVANTI'), 
+('AVENTON'), ('AVENUE'), ('AXXIS'), ('AZONIC'), ('BACINI'), ('BALANCE BUDDY'), ('BALLISTIC'), ('BAMBU BIKE BRASIL'), 
+('BAMF'), ('BANSHEE'), ('BASSO'), ('BEONE'), ('BERG'), ('BERGAMONT'), ('BERNARDI'), ('BERRIA '), ('BFOLD'), ('BH'), 
+('BIANCHI'), ('BIANINI'), ('BICIMOTO'), ('BIKE HAUS'), ('BIKE MANUFAKTUR'), ('BIKELAND'), ('BIOBIKE'), ('BIOMEGA '), 
+('BKL'), ('BLACK FIGHTER'), ('BLACK FLEA'), ('BLANK'), ('BLB'), ('BLITZ'), ('BLUE'), ('BMC'), ('BMW'), ('BOARDMAN'), 
+('BOHEMIAN'), ('BOMA'), ('BONETTI'), ('BOOMERANG'), ('BORNIA & COX'), ('BÖTTCHER'), ('BOTTECCHIA'), ('BRACICLO'), ('BRAND X'), 
+('BRAUNN'), ('BRAZIL ELECTRIC'), ('BREEZER'), ('BREMEM'), ('BRIDGESTON'), ('BRODIE'), ('BROMPTON'), ('BRUNETT'), ('BSA BICYCLES'), ('BT'), 
+('BTWIN'), ('BULLS'), ('BURNETT'), ('BXT'), ('CA NORCO'), ('CA ROCKY MOUNTAIN'), ('CADEX'), ('CAIÇARA'), ('CAIRU'), ('CAIXA'), 
+('CALFEE'), ('CALOI'), ('CALYPSO'), ('CANADIAN'), ('CANFIELD'), ('CANNON'), ('CANNONDALE'), ('CANYON'), ('CARDOSO'), 
+('CARRERA'), ('CARRY FREEDOM'), ('CEEPO'), ('CELL'), ('CERNUNNOS'), ('CERVÉLO'), ('CHARGE'), ('CHASE'), ('CHILLIBEANS'), ('CHRONOS'),
+('CHUMBA RACING'), ('CIA BRASIL'), ('CICLARE'), ('CICLÉG'), ('CILT'), ('CINELLI'), ('CIPOLLINI'), ('CITIZEN'), ('CITY KOM'), 
+('CLAUD BUTLER'), ('CLY'), ('COLLI'), ('COLNAGO'), ('COLNER'), ('COLONY'), ('COLOR BIKE'), ('COLOSSI'), ('COLUER'), ('COMMENCAL'), 
+('CONDOR'), ('CONOR'), ('CORIMA'), ('CORRATEC'), ('CORTINA'), ('COSTELO'), ('COZAC'), ('CREATE'), ('CREME'), ('CRONUS'), 
+('CRUZBIKE'), ('CUBE'), ('CULT'), ('CURRIE TECH'), ('CYCLETECH'), ('DA BOMB'), ('DACCORDI'), ('DAFRA'), ('DAHON'), ('DALANNIO'), 
+('DAMATTA'), ('DAWA'), ('DAWES'), ('DE ROSA'), ('DEAN'), ('DECATHLON'), ('DEDACCIAI'), ('DEKERF'), ('DEPAULA'), ('DEPEDAL'), 
+('DEVINCI'), ('DIAMONDBACK'), ('DINAMICA'), ('DINÂMICA'), ('DITEC'), ('DK'), ('DMS'), ('DNZ'), ('DOLAN'), ('DOSNOVENTA'), 
+('DOWNHILL'), ('DREAMBIKE'), ('DRÖSSIGER'), ('DTFLY'), ('DURBAN'), ('DYNAMICS'), ('E-CLUB'), ('E-LEEZE'), ('EAGLEBIKES'), 
+('EASTERN'), ('ECHO VINTAGE'), ('ECHOWELL'), ('ECOBIKE'), ('ECOS'), ('ECOSTART'), ('EDDY MERCKX'), ('EIGHTBIKE'), ('EIGHTBIKES'), 
+('EIGHTHINCH'), ('ELECTRA'), ('ELEEZE'), ('ELLEVEN'), ('ELLSWORTH'), ('ENDORPHINE'), ('ENDURO'), ('ENTERPRISE'), ('ESSENCIAL'), 
+('EUROBIKE'), ('EVO'), ('EVOKE'), ('F. MOSER'), ('FAILURE'), ('FALCON'), ('FELT'), ('FENIX'), ('FES'), ('FIB'), ('FICTION'), 
+('FIRETECK'), ('FIRMSTRONG'), ('FIRST'), ('FISCHER'), ('FIV5R'), ('FLEE'), ('FLYXII'), ('FOCUS'), ('FOES'), ('FOFFA'), ('FONDRIEST'), 
+('FORMAT'), ('FORME'), ('FORSS'), ('FOXXER'), ('FRAIDA'), ('FREEAGENT'), ('FSC'), ('FUGN'), ('FUJI'), ('FUN MOTORS'), ('FUNRIDE'), 
+('GALILEUS'), ('GALLO'), ('GAMA'), ('GARRA'), ('GARY FISHER'), ('GAZELLE'), ('GENERAL WINGS'), ('GENESIS'), ('GHAO'), ('GHOST'), 
+('GIANT'), ('GIC'), ('GILMEX'), ('GIORDANO '), ('GIOS'), ('GITANE'), ('GMC'), ('GO EASY'), ('GO SPORT'), ('GONEW'), ('GOOD NINE'), 
+('GÖRICK'), ('GRAZIELLA'), ('GRECG'), ('GRECO'), ('GREEN'), ('GROOVE'), ('GT'), ('GT MAX'), ('GT SUPER'), ('GTA'), ('GTI'), ('GTK'), 
+('GTS'), ('GTSM1'), ('GTW'), ('GTX'), ('GTZ'), ('GUNNAR'), ('GURU'), ('GW'), ('HAIBIKE'), ('HARO'), ('HARPY'), ('HASA'), ('HEBEI TONGYI'), 
+('HEILAND'), ('HERCULES'), ('HIATE'), ('HÍBRIDA'), ('HIGH ONE'), ('HOFFMAN'), ('HONGFU'), ('HÓRIZOM'), ('HOUSTON'), ('HOWMER'), ('HUBU'), 
+('HUFFY'), ('HUMMER'), ('HUPI'), ('HYPER'), ('IBIS'), ('IDENTITI'), ('INTENSE'), ('IRONHORSE'), ('J.SNOW'), ('JAMIS'), ('JAVA'), 
+('JBC PRO'), ('JBTECH'), ('JEEP'), ('JINDAL FINE INDUSTRIES'), ('JLS'), ('JNA'), ('JOHNNY LOCO'), ('K2'), ('KABUTO TAKARA'), ('KALF'), 
+('KAPA'), ('KARAKORAM'), ('KASINSKY'), ('KAWASAKI'), ('KEBI'), ('KEEN'), ('KENSTONE'), ('KENT'), ('KESTREL'), ('KHS'), ('KHT'), 
+('KIDDIMOTO'), ('KINESIS'), ('KINK'), ('KLEIN'), ('KMT'), ('KODE'), ('KOGA'), ('KOMBAT'), ('KONA'), ('KONNOR'), ('KRONUS'), ('KROSS'), 
+('KRS'), ('KSW'), ('KTM'), ('KULANA'), ('KUOTA'), ('KURUMA'), ('KUSTOMBIKE'), ('KYLIN'), ('LA BICI '), ('LA DRACO'), ('LAHSEN'), 
+('LAND ROVER'), ('LANGTU'), ('LAPIERRE'), ('LEE COUGAN'), ('LEMOND'), ('LEV'), ('LIKE'), ('LINUS'), ('LITESPEED'), ('LKSG'), 
+('LOOK'), ('LOTUS'), ('LTX'), ('LXR'), ('LYNSKEY'), ('M ROSSI'), ('M7'), ('MAGIAS'), ('MAGNA'), ('MANHATTAN'), ('MARIN BIKES'), 
+('MARSSTAR'), ('MARTIN'), ('MASCOT'), ('MASI'), ('MASTER BIKE'), ('MAZZA'), ('MBK'), ('MBL'), ('MDI'), ('MEGA'), ('MERAUD'), 
+('MERCIER'), ('MERIDA'), ('MERLIN'), ('MEYBO'), ('MIFA'), ('MITICAL'), ('MITSU'), ('MIYAMURA'), ('MIYATA'), ('MMR'), ('MOB'), ('MOBELE'), ('MODE'), 
+('MOMENTUM'), ('MONACO'), ('MONARK'), ('MONDAINE'), ('MONDRAKER'), ('MONGOOSE'), ('MONTAGNA'), ('MONTAGUE'), ('MONTY'), ('MOOD'), 
+('MOOTS'), ('MOPAR'), ('MORMAII'), ('MOSER'), ('MOSSO'), ('MOTACHIE'), ('MOTOBECANE'), ('MOULTON'), ('MOUNTAIN CYCLE'), ('MOVE YOUR LIFE'), 
+('MR. GUD'), ('MSC'), ('MUDDYFOX'), ('MULTILASER'), ('MURRAY'), ('MUSEEUW'), ('MUZZICYCLES'), ('MYMAX'), ('NATHOR'), ('NEASTY'), 
+('NEILPRYDE'), ('NESS'), ('NEW BIKE'), ('NEW RUN RIDE'), ('NEXT'), ('NIKONNA'), ('NINER'), ('NIRVE'), ('NISHIKI'), ('NORCO'), ('NOSTER'), 
+('NOVARA'), ('NOVELLO'), ('NS BIKES'), ('NUKEPOCTANE ONEROOF'), ('NUKEPROOF'), ('NUVEM'), ('OCEANO'), ('OCP'), ('OEM'), ('OGGI'), 
+('OLYMPIA'), ('ON ONE'), ('ONE'), ('OPEN'), ('OPTIMA'), ('ORBEA'), ('ORIGIN8'), ('ORTLER'), ('OX'), ('OX PRO'), ('OXER'), ('OZARK TRAIL'), 
+('PAGOTTI'), ('PALAZZO'), ('PASHLEY'), ('PEDALLA'), ('PEGASUS'), ('PEONY'), ('PERIFA'), ('PEUGEOT'), ('PHILLIPS'), ('PHOENIX'), ('PIERROT'), 
+('PINARELLO'), ('PINNACLE'), ('PIVOT'), ('PLANET X'), ('PLIAGE'), ('POLIMET'), ('PORTAL WHEELING'), ('PORTO SEGURO'), ('PREMIUM'), ('PREY'), 
+('PRINCE'), ('PRIORITY'), ('PRIVITERA'), ('PRO LITE'), ('PRO-X'), ('PROFESSIONAL'), ('PROFLEX'), ('PROMOUNTAIN'), ('PROSHOCK'), 
+('PUBLIC'), ('PUMMA'), ('PURE'), ('PYTHON'), ('PZZ RACING'), ('QUINTANA ROO'), ('R-WOOD'), ('RAASCH BIKE'), ('RACCER'), ('RADON '), ('RAF'), 
+('RAINBOW'), ('RAIO'), ('RALEIGH'), ('RALLY'), ('RAPTOR'), ('RAVA'), ('RC BIKES'), ('REDFOOT'), ('REDLAND'), ('REDLINE'), ('REDSTONE'), 
+('REEBOK'), ('REID'), ('RENAULT'), ('REPÚBLICA DA BICICLETA'), ('RETROSPEC'), ('REX'), ('RHARU'), ('RIDGEBACK'), ('RIDLEY'), 
+('RIESE & MÜLLER'), ('RINO'), ('RIOSOUTH'), ('RITCHEY'), ('RIVER'), ('ROCK GARDEN'), ('ROCKRIDER'), ('ROCKWAY'), ('ROCKY MOUNTAIN'), 
+('ROCOMO'), ('RONGHUI'), ('ROOSTER'), ('ROSA'), ('ROSSETTI'), ('ROTA HORIZONTE'), ('ROYALBABY'), ('ROYCE UNION'), ('RSC'), ('RUPTION'), 
+('RUSH'), ('S-WORKS'), ('SAIDX'), ('SALSA'), ('SAMCHULY BICYCLE'), ('SAMPSON'), ('SAMY'), ('SANMARCO'), ('SANTA CRUZ'), ('SANTA MONICA'), 
+('SANTINNI'), ('SANTOS'), ('SAVA'), ('SAVOY'), ('SCATTANTE'), ('SCHWINN'), ('SCIROCCO'), ('SCOOTER BRASIL'), ('SCOTT'), ('SCREPPER'), 
+('SCUD'), ('SENSE'), ('SERFAS'), ('SEROTTA'), ('SETTE'), ('SEVEN CYCLES'), ('SHIMANO'), ('SHINERAY'), ('SHOCKBLAZE'), ('SINTESI'), 
+('SIXXIS'), ('SKAPE'), ('SKODA'), ('SKY'), ('SKY LINE'), ('SLINGSHOT'), ('SLP'), ('SMART'), ('SMX'), ('SNOW'), ('SOBATO'), ('SOFTRIDE'), 
+('SOLYOM'), ('SOMA'), ('SOMAKRE'), ('SOUL'), ('SOUTH'), ('SOUTH BIKE'), ('SPACE'), ('SPECIAL LIFE'), ('SPECIALIZED'), ('SPEEDO'), ('SPIN'), 
+('SPINO'), ('SPORTIX'), ('SPRICK CYCLE'), ('STAMINA 29'), ('STARK'), ('STATE'), ('STATUS'), ('STEREO BIKES'), ('STEVENS'), ('STINGRAY'), 
+('STL'), ('STOLEN'), ('STONE'), ('STORCK'), ('STRADALLI'), ('STRADERA'), ('STRIDA'), ('SUMMER'), ('SUNBURST'), ('SUNDOWN'), ('SUNN'), 
+('SUNSET'), ('SUNTOUR'), ('SUPERO'), ('SUPERPEDESTRIAN'), ('SURGE'), ('SURLY'), ('SWIFT'), ('T-REX'), ('TAEQ'), ('TARGET'), ('TAURUS'), 
+('TAYTO'), ('TECBIKE'), ('TERN'), ('TEXAS RANGERS'), ('THUNDER'), ('TI CYCLES'), ('TIDEACE'), ('TIME'), ('TIRION'), ('TITO'), ('TITUS'), 
+('TOKEN'), ('TOMAC'), ('TOMMASO'), ('TOPBIKE'), ('TORELLI'), ('TORONTO'), ('TOSEEK'), ('TOTEM'), ('TOUR DE FRANCE'), 
+('TRACK & BIKES'), ('TRACTOR'), ('TRANSITION'), ('TRE3E'), ('TREK'), ('TRIAX'), ('TRIGON'), ('TRINX'), ('TRIUMPH'), ('TROPIX'), ('TROTZ'), 
+('TRUST'), ('TRYTO'), ('TSW'), ('TURNER'), ('TWIN SIX'), ('TWITTER'), ('TWO DOGS'), ('TWO HARD'), ('ULTIMATE'), ('UMF'), 
+('UNITED PELOTON'), ('UNIVEGA'), ('UPLAND'), ('URBANA'), ('VAN DESSEL'), ('VAXES'), ('VAZZO'), ('VECTOR'), ('VEHEN'), ('VELA'), 
+('VELLE'), ('VELOBUILD'), ('VELOFORCE'), ('VEMEX'), ('VENZO'), ('VERCELLI'), ('VERDEN'), ('VERSA'), ('VERTICAL'), ('VFS CUSTOMS'), 
+('VICINI'), ('VICINITECH'), ('VIKINGX'), ('VINER'), ('VIRTUE'), ('VISION'), ('VITUS'), ('VIVATEC'), ('VO2'), ('VO2BIKE'), ('VOLARE'), 
+('VOLTEC'), ('VOLUME'), ('VOOX'), ('VOYCE'), ('VZAN'), ('WATERFORD'), ('WATTS MOTORS'), ('WBT'), ('WENDY'), ('WENDYBIKE'), ('WETHEPEOPLE'), 
+('WHEELER'), ('WHYTE'), ('WILIER'), ('WINDSOR'), ('WINNING '), ('WNY'), ('WOIE'), ('WRP'), ('WS CRUISER'), ('X RACING'), ('X TERRA'), 
+('X-PLORE'), ('X-TREME'), ('XKS'), ('XTB'), ('XTGS'), ('YEDOO'), ('YETI'), ('YOELEO'), ('YOSEMITE'), ('YT INDUSTRIES'), ('YUNBIKE'), 
+('ZENITH'), ('ZEPPELIN'), ('ZIPP'), ('ZOHRER'), ('ZUMMI'), ('ZYMMER'), ('OUTRA');
+
+insert into bicicletas (MarcasId, Modelo, TiposId, Cor, Tamanho, 
+CambiosDianteirosId, CambiosTraseirosId, FreiosId, SuspensoesId, ArosId, 
+QuadrosId, Informacoes, AlertaRoubo, Vendendo) values 
+(96, 'Montana', 10, 'Preta/Branca', '18"', 3, 7, 2, 1, 6, 1, null, 0, 0),
+(96, 'Twister', 10, 'Amerela', '18"', 3, 7, 2, 1, 6, 1, null, 0, 0),
+(96, 'Andes', 10, 'Vermelha', '18"', 3, 7, 2, 2, 6, 1, 'Aro Aero', 0, 0),
+(96, '100 Sport', 10, 'Preta/Amarela', '18"', 3, 7, 2, 2, 6, 2, null, 0, 0),
+(96, 'TRS', 8, 'Azul', '18"', 3, 7, 2, 2, 6, 2, null, 0, 0),
+(96, 'T-Type', 8, 'Preta Fosco', '18"', 3, 7, 1, 3, 6, 2, 'Trocador Shimano Easy Fire, Câmbio traseiro Shimano', 0, 0),
+(96, '10', 4, 'Preta/Dourada', 'M', 2, 7, 5, 1, 1, 2, 'Câmbio traseiro Shimano, câmbio dianteiro Shimano, pedivela Shimano', 0, 0);
+
+insert into imagens (Imagem, BicicletasId) values 
+('caloi-montana.jpg', 1), ('caloi-twister.jpg', 2), ('caloi-andes.jpg', 3), ('caloi-100.jpg', 4),
+('caloi-trs.jpg', 5), ('caloi-ttype.jpg', 6), ('caloi-10.jpg', 7);
+
+insert into numerosseries (Numero, BicicletasId) values 
+('ABC1234CBA', 1), ('987ABCBR', 2), ('BR12345', 3), ('222AAA555', 4),
+('987654', 5), ('QWERTY123', 6), ('123ASDFG', 7);
+
+insert into pessoas (Nome, Email, ConfirmaEmail, Senha, ConfirmaSenha, 
+Endereco, Numero, Complemento, Cep, Bairro, Cidade, Estado, 
+Telefone, Celular, Cpf, DataNascimento, Genero, Imagem, 
+NomeContato, TelefoneContato, CelularContato, TipoUsuario) values 
+('Gustavo Teixeira', 'gustavoteixeira@email.com', 'gustavoteixeira@email.com', '12345678', '12345678', 
+'Rua Gustavo Teixeira', '111', 'Casa', '11111-111', 'Centro', 'João Pessoa', 14, 
+'(83) 1111-1111', '(83) 11111-1111', '111.111.111-11', '1996-06-10', 0, 'perfil01.jpg', 
+'Raul Castro', '(83) 1111-1111', '(83) 11111-1111', 0),
+('Benício Caldeira', 'benocaldeira@email.com', 'benocaldeira@email.com', '12345678', '12345678', 
+'Rua Benício Caldeira', '111', 'Bloco 1, Ap 3', '11111-111', 'Centro', 'Caraguatatuba', 24, 
+'(12) 1111-1111', '(12) 11111-3377', '111.111.111-11', '1987-07-20', 0, 'perfil02.jpg', 
+'Giovanna Nicole', null, '(12) 11111-1111', 0),
+('Mariah Moraes', 'marimoraes@email.com', 'marimoraes@email.com', '12345678', '12345678', 
+'Rua Mariah Moraes', '111', 'Casa', '11111-111', 'Centro', 'Dourados', 12, 
+'(67) 1111-1111', '(67) 11111-2030', '111.111.111-11', '1992-09-16', 1, 'perfil03.jpg', 
+'Olivia Liz', '(67) 1111-1111', null, 0),
+('Manuel Vicente', 'vicentemanuel@email.com', 'vicentemanuel@email.com', '12345678', '12345678', 
+'Rua Manuel Vicente', '111', 'Ap 8', '11111-111', 'Itaoca', 'Centro', 18, 
+'(21) 1111-1111', '(21) 11111-1111', '111.111.111-11', '1975-07-07', 0, 'perfil04.jpg', 
+'Stella Santos', '(21) 1111-1111', null, 0),
+('Rosa Carvalho', 'rosacarvalho@email.com', 'rosacarvalho@email.com', '12345678', '12345678', 
+'Avenida Rosa Carvalho', '111', null, '11111-111', 'Centro', 'Londrina', 15, 
+'(44) 1111-1111', '(44) 11111-1111', '111.111.111-11', '2000-12-04', 1, 'perfil05.jpg', 
+null, null, null, 0),
+('Rafaela Luana Lopes', 'rafalopes@email.com', 'rafalopes@email.com', '12345678', '12345678', 
+'Rua Rafaela Lopes', '111', 'Bloco C, Apartamento 5', '11111-111', 'Centro', 'Blumenau', 23, 
+'(47) 1111-1111', '(47) 11111-1111', '111.111.111-11', '1975-07-07', 1, 'perfil06.jpg', 
+'Davi Mota', '(47) 1111-1111', '(47) 11111-1111', 0),
+('Francisco Rodrigues', 'chicorois@email.com', 'chicorois@email.com', '12345678', '12345678', 
+'Rua Francisco Rodrigues', '111', 'Casa', '11111-111', 'Centro', 'Serra', 7, 
+'(27) 1111-1111', '(27) 11111-2889', '111.111.111-11', '1994-08-14', 0, 'perfil07.jpg', 
+'Mariane', '(27) 1111-1111', null, 0),
+('José Leandro Sales', 'zesales@email.com', 'zesales@email.com', '12345678', '12345678', 
+'Rua José Sales', '111', 'Casa', '11111-111', 'Centro', 'São Paulo', 24, 
+'(11) 1111-1111', '(11) 11111-1111', '111.111.111-11', '1987-01-26', 0, 'perfil08.jpg', 
+null, null, null, 0),
+('Danilo Baptista', 'danilobaptista@email.com', 'danilobaptista@email.com', '12345678', '12345678', 
+'Rua Danilo Baptista', '111', null, '11111-111', 'Centro', 'Bauru', 24, 
+'(14) 1111-1111', '(14) 11111-1111', '111.111.111-11', '1983-09-23', 0, 'perfil09.jpg', 
+'Otávio Baptista', null, '(11) 11111-1111', 0),
+('Natália Barros', 'nataliabarros@email.com', 'nataliabarros@email.com', '12345678', '12345678', 
+'Travessa Natália Barros', '111', 'Casa', '11111-111', 'Centro', 'Vitória de Santo Antão', 16, 
+'(81) 1111-1111', '(81) 11111-1111', '111.111.111-11', '1979-04-20', 1, 'perfil10.jpg', 
+'Leonardo Barros', '(81) 1111-1111', null, 0);
+
+insert into informacoesroubos (Relato, Local, Data, BicicletasId) values 
+('A bicicleta foi roubada na praça do centro, no período da manhã. 
+Ela estava trancada no porta bicicletas, junto com outras bicicletas.', 
+'Centro, São Paulo-SP', '2018-03-10', 1),
+('A bicicleta estava tancada no bicicletário do shopping, entre às 18h00 e 19h30.', 
+'Centro, São Paulo-SP', '2018-05-20', 3);
+
+insert into relatosroubos (Relato, Local, Data, PessoasId, InformacoesRoubosId) values 
+('A bicicleta esta à venda no classificados deste site, link:
+ http://exemplo.com.br/ciclismo/caloi-montana', 
+'Site Exemplo', '2018-05-25', 1, 1),
+('Vi o anúncio de venda da bicicleta no jornal da cidade, liguei e combinei com quem estava vendendo de ver a bicicleta. 
+Após verificar o número de série, vi que não era de quem estava vendendo.
+ Quem estava vendendo se chama João, celular (11)11111-1111', 
+'Centro, São Paulo-SP', '2018-08-29', 2, 2);
+
+insert into historicos (SituacaoAtual, DataAquisicao, DataTransferencia, BicicletasId, VendedorId, CompradorId) values 
+(0, '2017-01-20', '2018-02-15', 1, 1, 5),
+(0, '2016-10-05', '2018-05-10', 4, 2, 3);
