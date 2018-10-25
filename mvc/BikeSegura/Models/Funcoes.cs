bd21fa0bccacd9bce7d0e7e3c62db932d9cb2df5
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-10-25 11:58:20 -0300

    Classe Funções Criada
    Adicionado método de criptografia SHA512

---
+++ mvc/BikeSegura/Models/Funcoes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BikeSegura.Models
+{
+    public class Funcoes
+    {
+        // Criptografia SHA512
+        public static string SHA512(string valor)
+        {
+            UnicodeEncoding UE = new UnicodeEncoding();
+            byte[] HashValue, MessageBytes = UE.GetBytes(valor);
+            SHA512Managed SHhash = new SHA512Managed();
+            string strHex = "";
+
+            HashValue = SHhash.ComputeHash(MessageBytes);
+            foreach (byte b in HashValue)
+            {
+                strHex += String.Format("{0:x2}", b);
+            }
+            return strHex;
+        }
+    }
+}
