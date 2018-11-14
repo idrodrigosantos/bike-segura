5e7fe4d9eb9678c1877278e357b3906fac0f32d1
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-11-14 21:23:39 -0200

    Comentários Adicionados

--- mvc/BikeSegura/Models/Funcoes.cs
+++ mvc/BikeSegura/Models/Funcoes.cs
@@ -11,18 +11,29 @@ namespace BikeSegura.Models
     public class Funcoes
     {
         // Criptografia SHA512
+        // Receber valor da string, valor de entrada
         public static string SHA512(string valor)
         {
+            // Importa e cria nova instância
             UnicodeEncoding UE = new UnicodeEncoding();
+            // Declara array de bytes, tem hash de bytes, e uma mensagem de bytes
+            // Na variável UE gera GetBytes do valor que receber
             byte[] HashValue, MessageBytes = UE.GetBytes(valor);
+            // Declara SHA512, gera instância dele
             SHA512Managed SHhash = new SHA512Managed();
+            // Declara variável string
             string strHex = "";
 
+            // Chama HashValue, chama o ComputeHash que vai receber a mensagem(MessageBytes), 
+            //      que já tem bytes para gerar outros bytes
             HashValue = SHhash.ComputeHash(MessageBytes);
             foreach (byte b in HashValue)
             {
+                // Somar string strHex, atribuindo nela um formato específico
+                // Formato específico: ter hexadecimal em 2 caracteres
                 strHex += String.Format("{0:x2}", b);
             }
+            // Retorna string de hash
             return strHex;
         }
 
