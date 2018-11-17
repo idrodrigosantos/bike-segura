2df7931abce039263b8335ce2fcc1ca5d74b8df2
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-11-17 18:21:41 -0200

    Update

--- mvc/BikeSegura/App_Start/BundleConfig.cs
+++ mvc/BikeSegura/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace BikeSegura
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.3.1.min.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,7 +17,7 @@ namespace BikeSegura
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.8.3.js"));
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js"));
