8a4ae183188815baae84af3efbe0b8ad789bcb6f
Rodrigo Santos <rodriggodasilva@gmail.com>
2018-11-22 17:11:28 -0200

    Classe Alterada

--- mvc/BikeSegura/Models/Historicos.cs
+++ mvc/BikeSegura/Models/Historicos.cs
@@ -17,35 +17,32 @@ namespace BikeSegura.Models
         public OpcaoTransferencia TipoTransferencia { get; set; }
         public enum OpcaoTransferencia
         {
+            [Display(Name = "Proprietário Atual")]
+            ProprietarioAtual,
             [Display(Name = "Transferência Interna")]
             Interna,
             [Display(Name = "Transferência Externa")]
-            Externa,
-            [Display(Name = "Proprietário Atual")]
-            ProprietarioAtual
+            Externa            
         }
 
         [DisplayName("Data de Aquisição")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime DataAquisicao { get; set; }
+        public DateTime? DataAquisicao { get; set; }
 
         [DisplayName("Data de Transferência")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime DataTransferencia { get; set; }
+        public DateTime? DataTransferencia { get; set; }
 
-        [DisplayName("Bicicleta")]
-        [Required(ErrorMessage = "Bicicleta é obrigatório")]
+        [DisplayName("Bicicleta")]        
         public int BicicletasId { get; set; }
 
-        [DisplayName("Vendedor")]
-        [Required(ErrorMessage = "Vendedor é obrigatório")]
-        public int VendedorId { get; set; }
+        [DisplayName("Vendedor")]        
+        public int? VendedorId { get; set; }
 
-        [DisplayName("Comprador")]
-        [Required(ErrorMessage = "Comprador é obrigatório")]
-        public int CompradorId { get; set; }
+        [DisplayName("Comprador")]        
+        public int? CompradorId { get; set; }
 
         [EnumDataType(typeof(OpcaoStatusHistoricos))]
         public OpcaoStatusHistoricos Ativo { get; set; }
