29761906cb5c30d4b41bb012dae9a458ea288cd4
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-05-30 15:40:16 -0300

    Pacotes modificados

--- BikeSegura/BikeSegura/App_Start/BundleConfig.cs
+++ BikeSegura/BikeSegura/App_Start/BundleConfig.cs
@@ -9,19 +9,19 @@ namespace BikeSegura
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.3.1.intellisense.min.js",
+                        "~/Scripts/jquery-3.3.1.intellisense.js",
                         "~/Scripts/jquery-3.3.1.min.js",
                         "~/Scripts/jquery-3.3.1.slim.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate-vsdoc.min.js",
+                        "~/Scripts/jquery.validate-vsdoc.js",
                         "~/Scripts/jquery.validate.min.js",
                         "~/Scripts/jquery.validate.unobtrusive.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-2.8.3.min.js"));
+                        "~/Scripts/modernizr-2.8.3.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js"));
@@ -38,7 +38,7 @@ namespace BikeSegura
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/meu-estilo.css",
-                      "~/Content/fontawesome/css/all.min.css",                      
+                      "~/Content/fontawesome/all.min.css",
                       "~/Content/toastr.min.css"));
         }
     }
