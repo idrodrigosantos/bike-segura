2a342050ab233e01c5cfe207e4a11d9e5d1f473c
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-06-01 21:40:33 -0300

    Update

--- bike-segura/BikeSegura/Properties/AssemblyInfo.cs
+++ bike-segura/BikeSegura/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BikeSegura")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2fabe278-0740-42ec-a25f-f84d86e34b34")]
+[assembly: Guid("0fa26afc-dbb7-4f9f-a2d4-b8d1937b8df9")]
 
 // Version information for an assembly consists of the following four values:
 //
